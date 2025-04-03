40d80e87ade277da2b9d17dbd7fc6cc4a20e7d08
Tavish Chawla <tchawla827@gmail.com>
2025-04-03 13:54:17 +0530

    changed to online sql hosting

--- FilmFair/wsgi.py
+++ FilmFair/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BoxOffice.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FilmFair.settings')
 
 application = get_wsgi_application()
